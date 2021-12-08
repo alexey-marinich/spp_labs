6d87dbf7ea01ad59336105d40fc584f0778ecbc4
Alexey Marinich <71971483+alexey-marinich@users.noreply.github.com>
2021-12-09 02:13:12 +0300

    Add files via upload

---
+++ lr1/lr1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace lr1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TaskQueue taskQueue = new TaskQueue(5);
+			for (int i = 0; i < 5; i++)
+			{
+				taskQueue.EnqueueTask(count);
+			}
+		}
+		static void count()
+		{
+			int j = 1;
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine("Поток id{0} >> #{1}", Thread.CurrentThread.ManagedThreadId, j);
+				j++;
+				Thread.Sleep(50);
+			}
+		}
+	}
+}
---
+++ lr1/lr1/TaskQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace lr1
+{
+    public delegate void TaskDelegate();
+
+    class TaskQueue
+    {
+        static object locker = new object();
+        BlockingCollection<TaskDelegate> Tasks = new BlockingCollection<TaskDelegate>(new ConcurrentQueue<TaskDelegate>());
+
+        public TaskQueue(int queueCount)
+        {
+            for (int i = 0; i < queueCount; i++)
+            {
+                var thread = new Thread(ThreadWork); 
+                thread.Start();
+            }
+        }
+
+        public void ThreadWork()
+        {
+
+            while (true)
+            {
+              //  lock(locker){ 
+                var task = Tasks.Take();
+                    task();
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "  id");
+               // }
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            this.Tasks.Add(task);
+        }
+    }
+}
