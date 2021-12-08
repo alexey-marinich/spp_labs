38ac307a61240d1d66218edb06a1399b9cfc997e
Alexey Marinich <71971483+alexey-marinich@users.noreply.github.com>
2021-12-09 02:16:39 +0300

    Add files via upload

---
+++ laba_6/laba_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace laba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogBuffer logBuffer = new LogBuffer(5, 2000, "D:/file.txt");
+
+            for(int i = 0; i < 200; i++)
+            {
+                logBuffer.Add(i.ToString());
+            }
+            logBuffer.Close();
+            Console.WriteLine("Готово");
+            Console.ReadLine();
+        }
+    }
+}
